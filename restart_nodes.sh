fed8d7b8bd3e16c2f48fe67765a0b9e7b1fd38fe
eddy0215 <eddy0215@kakao.com>
2025-10-28 13:47:15 +0900

    아두이노 csi루프백 비디오채널로 바꿈re

--- restart_nodes.sh
+++ restart_nodes.sh
@@ -2,9 +2,9 @@
 # =========================================
 # ROS2 노드 종료 후 재실행 스크립트
 # =========================================
-
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
+source ~/ros2_ws/install/setup.bash
 
 # --------------------------
 # 1️⃣ 실행 중인 관련 ROS2 노드 종료
@@ -28,7 +28,7 @@ ros2 run gpio_node motor_node &
 ros2 run gpio_node ultrasonic_node &
 
 # opencv_cam_main (예: index 1, 640x480, 30fps)
-ros2 run opencv_cam opencv_cam_main --ros-args -p index:=1 -p width:=640 -p height:=480 -p fps:=30 &
+ros2 run opencv_cam opencv_cam_main --ros-args -p index:=3 -p width:=640 -p height:=480 -p fps:=30 &
 
 echo "All ROS2 nodes restarted."
 
